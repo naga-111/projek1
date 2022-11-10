fd4a8b322c48a0bca2cc1a6029a13d69ad7c916d
naga-111 <115853360+naga-111@users.noreply.github.com>
2022-11-10 19:56:36 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+	$db = mysqli_connect('localhost', 'root', '', 'maklumat')
+	
+	//initilize variable
+	$id = 0;
+	$nama = "";
+	$nokp = "";
+	$tarikhlahir = "";
+	$umur = "";
+	$alamat = "";
+	$sijil = "";
+?>
---
+++ create.php
@@ -0,0 +1,23 @@
+<?php
+
+	if(isset($_POST['create']))
+	{
+		$conn = mysqli_connect("localhost","root","","maklumat");//konek
+		
+		//tangkap brg2 dari form
+		$fname = mysqli_real_escape_string($conn, $_POST['fname']);
+		$nokp = mysqli_real_escape_string($conn, $_POST['ic']);
+		$tarikh = mysqli_real_escape_string($conn, $_POST['date']);
+		$umur = mysqli_real_escape_string($conn, $_POST['number']);
+		$alamat = mysqli_real_escape_string($conn, $_POST['address']);
+		
+		//ayat2 cinta sql
+		$query = "INSERT INTO data(nama,nokp,tarikhlahir,umur,alamat) VALUES ('$fname','$nokp','$tarikh','$umur','$alamat')";
+		$result = mysqli_query($conn,$query);//kasi jalan tu sql
+		
+		header("location:idex.html");//ciao balik p idex.html
+		
+		
+	}
+
+?>
